6441d5c0571905f2f0d0ecf7dbf4fa7bc827e3fa
Poke1330 <104279424+Poke1330@users.noreply.github.com>
2022-04-25 13:46:57 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require ('http');
+const fs = require('fs');
+const os = require('os');
+const dir =  './pages/';
+const port = 8080;
+
+const server =http.createServer( (req, res) =>{
+    
+ if(req.url ==='/'){
+     res.statusCode = 200;
+     res.setHeader('contentType', 'text/html');
+     render(res,'index.html');
+ }
+ else if(req.url ==='/about'){
+    res.statusCode = 200;
+    res.setHeader('contentType', 'text/html');
+    render(res,'about.html');
+}
+ else if(req.url ==='/sys'){
+    res.statusCode = 201;
+    res.setHeader('contentType', 'text/plain');
+    res.end('Your OS info has been saved successfully!');
+
+    const content = {
+        hostname:(os.hostname()),
+       platform:(os.platform()),
+        arch:(os.arch()),
+        cpu:(os.cpus().length),
+        networkinterface:(os.networkInterfaces()),
+        uptime:(os.uptime()),
+    };
+
+    
+fs.writeFile('./osinfo.json', JSON.stringify(content, null, 2), err =>{
+if (err){
+    console.error(err);
+    return;
+}
+
+
+}
+);
+}
+else {
+        res.statusCode = 404;
+        res.setHeader('contentType', 'text/html');
+        render(res,'404.html');
+    
+}
+}).listen(port, ()=>{
+    console.log( `http://localhost:${port}`);
+})
+
+ const render =(res, file) =>{
+fs.readFile(dir+file, (err, data) =>{
+    if(err){
+        res.writeHead(404, {'ContentType': 'text/html'});
+       render('404.html')   
+   }
+    res.writeHead(200,{'ContentType': 'text/html'});
+    return res.end(data);
+});
+}
